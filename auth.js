a2c3005982c6bdcd0d8e2d380d63186e96935b70
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-06-30 12:41:23 +0200

    add JSDoc documentation

--- auth.js
+++ auth.js
@@ -1,29 +1,38 @@
-const jwtSecret = "your_jwt_secret"; //must be same key used in JWTStategy
+const jwtSecret = 'your_jwt_secret'; //must be same key used in JWTStategy
 
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
-require("./passport"); //Your local passport file
+require('./passport'); //Your local passport file
 
-let generateJWTToken = user => {
+let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username,
-    expiresIn: "7d",
-    algorithm: "HS256"
+    expiresIn: '7d',
+    algorithm: 'HS256',
   });
 };
 
-//POST login.
-module.exports = router => {
-  router.post("/login", (req, res) => {
-    passport.authenticate("local", { session: false }, (error, user, info) => {
+/**
+ * Route logging in the user.
+ * Handles user login, generating a jwt upon login
+ * Endpoint: /login
+ * @method POST
+ * @function
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @returns {JSON} - user data, jwt
+ */
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: "Something is wrong.",
-          user: user
+          message: 'Something is wrong.',
+          user: user,
         });
       }
-      req.login(user, { session: false }, error => {
+      req.login(user, { session: false }, (error) => {
         if (error) {
           res.send(error);
         }
